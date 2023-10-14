7ebd5b374cf188743cefd13c9b575344ff6f902a
aung-myat-oo <aungmyatoo2025@gmail.com>
2023-10-14 21:51:47 +0630

    push

--- script.js
+++ script.js
@@ -2,19 +2,25 @@ const day = document.getElementsByClassName("day")[0];
 const hour = document.getElementsByClassName("hour")[0];
 const minute = document.getElementsByClassName("minute")[0];
 const second = document.getElementsByClassName("second")[0];
-
+const contentBox=document.getElementsByClassName("contentBox")[0];
 const anniversaryDate = "15 april 2024";
 
 function countTime() {
   const currentTIme = new Date();
   const leftTime = new Date(anniversaryDate);
   const seconds = (leftTime - currentTIme) / 1000;
-  const days = Math.floor(seconds / 3600 /24);
-  const hours=Math.floor(seconds/3600)%24;
-  const minutes=Math.floor(seconds/60)%60;
-  const leftSeconds=Math.floor(seconds)%60
-  // console.log(minutes)
-  console.log(days,hours,minutes,leftSeconds)
+  const days = Math.floor(seconds / 3600 / 24);
+  const hours = Math.floor(seconds / 3600) % 24;
+  const minutes = Math.floor(seconds / 60) % 60;
+  const leftSeconds = Math.floor(seconds) % 60;
+  day.textContent = days;
+  hour.textContent = hours < 10 ? "0" + hours : hours;
+  minute.textContent = minutes < 10 ? "0" + minutes : minutes;
+  second.textContent = leftSeconds < 10 ? "0" + leftSeconds : leftSeconds;
+  if (days === 0 && hours === 0 && minutes === 0 && leftSeconds === 0) {
+    contentBox.classList.remove("hide")
+  }
 }
+
 countTime();
-setInterval(countTime,1000)
+setInterval(countTime, 1000);
